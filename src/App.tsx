2458d05dc559b26d7aa70ef514956319ea45db5b
Maximiliano Chavez <104655710+Maxigamble@users.noreply.github.com>
2023-06-28 12:16:19 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  const Home = () => <h1>Bienvenido</h1>;
+  const Contacto = () => <h1>Contacto</h1>;
+
+  return (
+    <>
+      <Home />
+      <Contacto />
+    </>
+  );
+}
+
+export default App;
